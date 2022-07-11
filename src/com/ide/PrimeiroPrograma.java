91f6998a188ffe0038fd98f57859a4b743d41576
ThatyannePrado <thatyanneprado@poli.ufrj.br>
2022-07-11 20:52:53 -0300

    first commit

---
+++ src/com/ide/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package com.ide;
+
+import com.ide.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        /*int a = 2;
+        int b = 3;
+        System.out.println("Hello World"+(a+b));*/
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+
+    }
+}
